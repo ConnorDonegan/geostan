c61a5c9ebe3c2c90d0208718d4bb71a84e4cd541
Connor Donegan <connor.donegan@utdallas.edu>
2020-08-02 15:06:31 -0500

    update ME names

--- R/prep-me-data.R
+++ R/prep-me-data.R
@@ -53,7 +53,7 @@ prep_me_data <- function(ME, family, x) { # for x pass in x.list$x
       # start building the return data list
    me.list <- list(offset_me = offset_me,
                    model_offset = model_offset)
-  if (is.null(ME$ME)) {
+  if (is.null(ME$se)) {
       # in this case there is just an offset me model but not ME.X
       x_obs <- x
       dx_obs <- ncol(x_obs)
@@ -79,10 +79,10 @@ prep_me_data <- function(ME, family, x) { # for x pass in x.list$x
           me.list <- c(me.list, me.x.list)
  } else {
      ## now deal with observational error in covariates, if any
-        if (!inherits(ME$ME, "data.frame")) stop("ME must be a list in which the element named ME is of class data.frame, containing standard errors for the observations.")
-        if  (!all(names(ME$ME) %in% names(x.df))) stop("All column names in ME$ME must be found in the model matrix (from model.matrix(formula, data)). This error may occur if you've included some kind of data transformation in your model formula, such as a logarithm or polynomial, which is not supported for variables with sampling/measurement error.")
+        if (!inherits(ME$se, "data.frame")) stop("ME must be a list in which the element named ME is of class data.frame, containing standard errors for the observations.")
+        if  (!all(names(ME$se) %in% names(x.df))) stop("All column names in ME$se must be found in the model matrix (from model.matrix(formula, data)). This error may occur if you've included some kind of data transformation in your model formula, such as a logarithm or polynomial, which is not supported for variables with sampling/measurement error.")
         if (length(ME$bounded)) {
-            if (length(ME$bounded) != ncol(ME$ME)) stop("ME mis-specified: bounded must be a vector with one element per column in the ME dataframe.")
+            if (length(ME$bounded) != ncol(ME$se)) stop("ME mis-specified: bounded must be a vector with one element per column in the ME dataframe.")
             bounded <- which(ME$bounded == 1)
             not.bounded <- which(ME$bounded != 1)
             if (length(ME$bounds)) {
@@ -90,31 +90,31 @@ prep_me_data <- function(ME, family, x) { # for x pass in x.list$x
                 bounds <- ME$bounds
             }
         } else {
-            bounded <- integer(0) #rep(0, times = ncol(ME$ME))
-            not.bounded <- 1:ncol(ME$ME) #rep(1, times = ncol(ME$ME))
+            bounded <- integer(0) #rep(0, times = ncol(ME$se))
+            not.bounded <- 1:ncol(ME$se) #rep(1, times = ncol(ME$se))
         }           
            # gather any/all variables without ME
-          x_obs_idx <- as.array( which( !names(x.df) %in% names(ME$ME) )) 
+          x_obs_idx <- as.array( which( !names(x.df) %in% names(ME$se) )) 
           x_obs <- as.data.frame(x.df[, x_obs_idx])
           dx_obs <- ncol(x_obs)
           # now X.me needs to be parsed into bounded/non-bounded variables and ordered as x
-          nm_me_unbounded <- names(ME$ME)[not.bounded]
+          nm_me_unbounded <- names(ME$se)[not.bounded]
           x_me_unbounded <- data.frame( x.df[, nm_me_unbounded] )
           names(x_me_unbounded) <- nm_me_unbounded
           x_me_unbounded_order <- na.omit( match(names(x.df), names(x_me_unbounded)) )
           x_me_unbounded <- data.frame(x_me_unbounded[, x_me_unbounded_order])
           dx_me_unbounded <- ncol(x_me_unbounded)
-          sigma_me_unbounded <- as.matrix(ME$ME[,not.bounded], nrow = n)
+          sigma_me_unbounded <- as.matrix(ME$se[,not.bounded], nrow = n)
           sigma_me_unbounded <- as.matrix(sigma_me_unbounded[, x_me_unbounded_order], nrow = n)
           x_me_unbounded_idx <- as.array( which( names(x.df) %in% nm_me_unbounded ))
           
-          nm_me_bounded <- names(ME$ME)[bounded]
+          nm_me_bounded <- names(ME$se)[bounded]
           x_me_bounded <- data.frame( x.df[, nm_me_bounded] )
           names(x_me_bounded) <- nm_me_bounded
           x_me_bounded_order <- na.omit( match(names(x.df), names(x_me_bounded)) )
           x_me_bounded <- as.matrix(x_me_bounded[, x_me_bounded_order], nrow = n)
           dx_me_bounded <- ncol(x_me_bounded)
-          sigma_me_bounded <- as.matrix(ME$ME[,bounded], nrow = n)
+          sigma_me_bounded <- as.matrix(ME$se[,bounded], nrow = n)
           sigma_me_bounded <- as.matrix(sigma_me_bounded[, x_me_bounded_order], nrow = n)
           x_me_bounded_idx <- as.array( which( names(x.df) %in% nm_me_bounded ))
           # handle unused parts
